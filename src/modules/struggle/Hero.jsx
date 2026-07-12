3b688f63ab2cb046befab524a66232f133ba5c5c
Muhammad Zeeshan Akram <chzeeshanszforever099@gmail.com>
2026-07-12 13:19:12 +0500

    working on struggle page

--- src/modules/struggle/Hero.jsx
+++ src/modules/struggle/Hero.jsx
@@ -4,25 +4,29 @@ import React from 'react'
 const Hero = () => {
   return (
     <>
-    <div className='flex items-center justify-between'>
-      <div className='border border-red-500 w-1/2'>
+    <div className='flex flex-col items-center gap-4 lg:flex-row lg:items-stretch lg:justify-between'>
+      <div className='w-full lg:w-1/2'>
            <Image
             src="/owner%20images/struggle.png"
             alt="missing struggle page image"
-            width={100}
-            height={100}
+            width={1672}
+            height={941}
             priority
-            className="w-full h-auto"
+            quality={100}
+            sizes="(max-width: 1024px) 100vw, 50vw"
+            className="h-auto w-full"
             />
       </div>
-      <div className='border border-green-500 w-1/2'>
+      <div className='w-full lg:w-1/2'>
            <Image
             src="/struggle1.png" 
             alt="missing struggle page image"
-            width={100}
-            height={100}
+            width={1672}
+            height={941}
             priority
-            className="w-full h-auto"
+            quality={100}
+            sizes="(max-width: 1024px) 100vw, 50vw"
+            className="h-auto w-full"
             />
       </div>
     </div>
