8086f23a8d71a53f0eb2bdb52ea47b240b8be4c9
Muhammad Zeeshan Akram <chzeeshanszforever099@gmail.com>
2026-07-14 09:47:23 +0500

    working on live classes page

---
+++ src/modules/live-classes/TrustedCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TrustedCard = () => {
+  return (
+   <>
+   
+   </>
+  )
+}
+
+export default TrustedCard
--- src/modules/live-classes/index.jsx
+++ src/modules/live-classes/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from './Hero'
 import Features from './Features'
 import Teachers from './Teachers'
+import TrustedCard from './TrustedCard'
 const index = () => {
   return (
     <>
@@ -9,6 +10,7 @@ const index = () => {
       <Hero/>
       <Features/>
       <Teachers/>
+      <TrustedCard/>
     </div>
     </>
   )
