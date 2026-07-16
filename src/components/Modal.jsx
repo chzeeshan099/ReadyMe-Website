8588daf7b35162eb4a7214f72bc20e982909d87e
Muhammad Zeeshan Akram <chzeeshanszforever099@gmail.com>
2026-07-16 16:55:11 +0500

    add model

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -8,14 +8,18 @@ import { IoClose } from "react-icons/io5";
 
 
 export default function CustomModal({ title, isOpen, onClose, children }) {
- 
+  
 
   return (
    <>
-    <Modal isOpen={isOpen} onClose={onClose}>
+    <Modal
+      isOpen={isOpen}
+      onClose={onClose}
+      overlayClassName="bg-[#020611]/45 backdrop-blur-xl"
+    >
         <div className="m-auto px-7 pt-6 pb-8 bg-lightBlue rounded-2xl max-h-[90vh] overflow-auto custom-scrollbar">
           <div className="mb-7 flex items-center justify-between">
-            <Title as="h3" className="text-blue">{title}</Title>
+            <Title as="h3" className="">{title}</Title>
             <ActionIcon
               size="sm"
               variant="text"
